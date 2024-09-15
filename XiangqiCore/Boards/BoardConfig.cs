c6fa84224bf2d649657fa61af5397c8ca610b596
chijason99 <chijason99@gmail.com>
2024-09-15 08:56:36 +0100

    in code documentation for board config

--- XiangqiCore/Boards/BoardConfig.cs
+++ XiangqiCore/Boards/BoardConfig.cs
@@ -3,13 +3,34 @@ using XiangqiCore.Pieces;
 using XiangqiCore.Pieces.PieceTypes;
 
 namespace XiangqiCore.Boards;
+
+/// <summary>
+/// Represents the configuration of a game board.
+/// </summary>
 public class BoardConfig
 {
-    public Dictionary<Coordinate, Piece> PiecesToAdd { get; } = [];
+	/// <summary>
+	/// Gets the dictionary of pieces to add to the board.
+	/// </summary>
+	public Dictionary<Coordinate, Piece> PiecesToAdd { get; } = new Dictionary<Coordinate, Piece>();
 
-    public void AddPiece(PieceType targetPieceType, Side targetColor, Coordinate targetCoordinate)
-        => PiecesToAdd[targetCoordinate] = PieceFactory.Create(targetPieceType, targetColor, targetCoordinate);
+	/// <summary>
+	/// Adds a piece of the specified type and color to the specified coordinate.
+	/// </summary>
+	/// <param name="targetPieceType">The type of the piece to add.</param>
+	/// <param name="targetColor">The color of the piece to add.</param>
+	/// <param name="targetCoordinate">The coordinate where the piece should be added.</param>
+	public void AddPiece(PieceType targetPieceType, Side targetColor, Coordinate targetCoordinate)
+	{
+		PiecesToAdd[targetCoordinate] = PieceFactory.Create(targetPieceType, targetColor, targetCoordinate);
+	}
 
-    public void AddRandomPiece(Coordinate targetCoordinate)
-        => PiecesToAdd[targetCoordinate] = PieceFactory.CreateRandomPiece(targetCoordinate);
+	/// <summary>
+	/// Adds a random piece to the specified coordinate.
+	/// </summary>
+	/// <param name="targetCoordinate">The coordinate where the piece should be added.</param>
+	public void AddRandomPiece(Coordinate targetCoordinate)
+	{
+		PiecesToAdd[targetCoordinate] = PieceFactory.CreateRandomPiece(targetCoordinate);
+	}
 }
