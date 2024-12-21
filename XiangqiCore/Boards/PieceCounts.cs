c84a29c7bc9ef4535849f8ba793e23ec8a03cdb1
chijason99 <chijason99@gmail.com>
2024-12-21 12:24:36 +0000

    feat: PieceCounts for randomising the starting position

---
+++ XiangqiCore/Boards/PieceCounts.cs
@@ -0,0 +1,12 @@
+﻿using XiangqiCore.Misc;
+using XiangqiCore.Pieces.PieceTypes;
+
+namespace XiangqiCore.Boards;
+
+public record PieceCounts(Dictionary<PieceType, int> RedPieces, Dictionary<PieceType, int> BlackPieces)
+{
+	public int GetPieceCount(PieceType pieceType, Side side)
+	{
+		return side == Side.Red ? RedPieces.GetValueOrDefault(pieceType, 0) : BlackPieces.GetValueOrDefault(pieceType, 0);
+	}
+}
