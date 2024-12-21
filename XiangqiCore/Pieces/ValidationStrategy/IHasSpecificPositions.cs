fc4d929a235eaf3ed09afccfe7f85627f949e148
chijason99 <chijason99@gmail.com>
2024-12-21 13:18:40 +0000

    feat: new interface for bishops and advisors which have specific locaitons that they can move to

---
+++ XiangqiCore/Pieces/ValidationStrategy/IHasSpecificPositions.cs
@@ -0,0 +1,15 @@
+﻿using XiangqiCore.Misc;
+
+namespace XiangqiCore.Pieces.ValidationStrategy;
+
+/// <summary>
+/// For Bishop and Advisor, they have specific positions that they can move to, so they need to implement this interface
+/// </summary>
+public interface IHasSpecificPositions
+{
+	/// <summary>
+	/// Get the possible positions that the piece can move to
+	/// </summary>
+	/// <returns>An array of coordinates that the piece can move to</returns>
+	Coordinate[] GetSpecificPositions(Side side);
+}
