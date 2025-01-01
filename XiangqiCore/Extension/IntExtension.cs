a5c18b38b5273c56f5bfb3b5fb16505ffa307dac
chijason99 <chijason99@gmail.com>
2025-01-01 00:32:16 +0000

    feat: to Chinese Numeral extension

--- XiangqiCore/Extension/IntExtension.cs
+++ XiangqiCore/Extension/IntExtension.cs
@@ -8,4 +8,19 @@ public static class IntExtension
 
     public static int ConvertToColumnBasedOnSide(this int originalValue, Side side)
         => side == Side.Red ? 10 - originalValue : originalValue;
+
+    public static string ToChineseNumeral(this int originalValue)
+		=> originalValue switch
+		{
+			1 => "一",
+			2 => "二",
+			3 => "三",
+			4 => "四",
+			5 => "五",
+			6 => "六",
+			7 => "七",
+			8 => "八",
+			9 => "九",
+			_ => throw new ArgumentException("Invalid Column Value")
+		};
 }
