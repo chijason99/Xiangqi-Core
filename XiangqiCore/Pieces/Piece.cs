4b52120696771f685a62fe3d9c723e0a2b24a489
chijason99 <chijason99@gmail.com>
2025-02-01 12:58:25 +0000

    style: line breaks

--- XiangqiCore/Pieces/Piece.cs
+++ XiangqiCore/Pieces/Piece.cs
@@ -23,12 +23,19 @@ public abstract class Piece(Coordinate coordinate, Side side)
     }
 
 	public Coordinate Coordinate { get; private set; } = coordinate;
+
     public Side Side { get; init; } = side;
+
     public abstract IValidationStrategy ValidationStrategy { get; }
+
     public virtual int[] GetAvailableRows() => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
     public virtual int[] GetAvailableColumns() => [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
     public abstract PieceType PieceType { get; }
+
     public abstract char FenCharacter { get; }
+
     public char GetFenCharacter => Side == Side.Red ? char.ToUpper(FenCharacter) : Side == Side.Black ? char.ToLower(FenCharacter) : FenCharacter;
 
     public virtual Coordinate GetDestinationCoordinateFromNotation(MoveDirection moveDirection, int fourthCharacterInNotation)
