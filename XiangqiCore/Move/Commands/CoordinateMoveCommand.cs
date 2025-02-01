502501891eee4523ba403b996ce8e1e5a9cb9898
chijason99 <chijason99@gmail.com>
2025-02-01 13:15:50 +0000

    feat: remove board as a dependency for move command

--- XiangqiCore/Move/Commands/CoordinateMoveCommand.cs
+++ XiangqiCore/Move/Commands/CoordinateMoveCommand.cs
@@ -5,31 +5,28 @@ using XiangqiCore.Move.MoveObjects;
 namespace XiangqiCore.Move.Commands;
 
 public class CoordinateMoveCommand(
-	Board board, 
 	Coordinate from, 
 	Coordinate to,
 	Side sideToMove) : IMoveCommand
 {
-	private readonly Board _board = board;
 	private readonly Coordinate _from = from;
 	private readonly Coordinate _to = to;
 	private readonly Side _sideToMove = sideToMove;
 
 	public MoveHistoryObject MoveHistoryObject { get; private set; }
 
-
-	public MoveHistoryObject Execute()
+	public MoveHistoryObject Execute(Board board)
 	{
-		MoveHistoryObject moveHistoryObject = _board.MakeMove(_from, _to, _sideToMove);
+		MoveHistoryObject moveHistoryObject = board.MakeMove(_from, _to, _sideToMove);
 
 		MoveHistoryObject = moveHistoryObject;
 
 		return moveHistoryObject;
 	}
 
-	public MoveHistoryObject Undo()
+	public MoveHistoryObject Undo(Board board)
 	{
-		_board.UndoMove(MoveHistoryObject);
+		board.UndoMove(MoveHistoryObject);
 
 		return MoveHistoryObject;
 	}
