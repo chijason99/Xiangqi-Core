7cb50c7992ee6d714aac351d34c35b9698223f29
chijason99 <chijason99@gmail.com>
2025-02-01 13:16:05 +0000

    feat: simple move object

---
+++ XiangqiCore/Move/MoveObjects/SimpleMoveObject.cs
@@ -0,0 +1,13 @@
+﻿using XiangqiCore.Misc;
+using XiangqiCore.Pieces;
+
+namespace XiangqiCore.Move.MoveObjects;
+
+/// <summary>
+/// Represents a simple move object, used for making/undoing moves in place, instead of deep cloning the piece array.
+/// </summary>
+public record class SimpleMoveObject(
+	Coordinate StartingPosition, 
+	Coordinate Destination, 
+	Piece PieceMoved, 
+	Piece PieceCaptured);
